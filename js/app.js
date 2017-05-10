7aaeb4b2bd1e62036e2cfed19a30de3f7ef0223b
emaquino44 <prexy44@yahoo.com>
2017-05-09 18:22:22 -0700

    lab done

--- js/app.js
+++ js/app.js
@@ -1,5 +1,33 @@
-angular.module('GiphyApp', [])
+/* globals angular */
+var GiphyApp = angular.module("GiphyApp", []);
 
-.run(function() {
-  console.log('App has loaded!');
-});
+GiphyApp.controller('Giphy', ['$scope', '$http', function($scope, $http) {
+
+    $scope.giphyList = [];
+
+    $scope.search = function() {
+        var req = {
+            url: 'http://api.giphy.com/v1/gifs/search',
+            method: 'GET',
+            params: {
+                q: $scope.searchTerm,
+                api_key: 'dc6zaTOxFJmzC',
+                limit: 10
+            }
+        };
+
+        $http(req).then(function(res) {
+            $scope.giphyList = res.data.data;
+        });
+    };
+
+    var debounce = null;
+    $scope.$watch('searchTerm', function(newVal, oldVal) {
+        if (debounce) clearTimeout(debounce);
+        debounce = setTimeout(function() {
+            // console.log('Ran Search');
+            $scope.search();
+        }, 1000);
+    });
+
+}]);
